7d7bcb7aa6e04474fb049790fef0c2b34989a311
MoonInbae <mib010213@gmail.com>
2022-06-13 19:20:23 +0900

    동네생활 레이아웃
    동네생활 레이아웃

--- frontend/src/screens/Community.js
+++ frontend/src/screens/Community.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+
+const { width : SCREEN_WIDTH } = Dimensions.get("window");
 
 const Container = styled.View`
   flex: 1;
@@ -8,12 +11,56 @@ const Container = styled.View`
   background-color: #E7EBF4;
 `;
 
+const List = styled.ScrollView`
+  flex: 1;
+`;
+
 const Community = () => {
   return (
-    <Container>
-      동네생활
-    </Container>
+    <List>
+      <Container>
+      {/* <Container>
+        <Text>동네생활</Text>
+      </Container> */}
+        <View style={styles.content}>
+          <TouchableOpacity>
+            <View style={styles.titlePart}>
+              <Text style={styles.type}>(공지) </Text>
+              <Text style={styles.title}>제목</Text>
+            </View>
+            <Text style={styles.writer}>관리자</Text>
+          </TouchableOpacity>
+        </View>
+        
+      </Container>
+    </List>
   );
 };
+
+const styles = StyleSheet.create({
+  content: {
+    // flex: 1,
+    backgroundColor: 'white',
+    borderRadius : 10,
+    borderWidth : 1,
+    borderColor : 'skyblue',
+    padding: 10,
+    width: SCREEN_WIDTH/10*9,
+    margin: 3,
+  },
+  titlePart: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  type: {
+    color: 'red',
+  },
+  title: {
+
+  },
+  writer: {
+    textAlign: 'right',
+  },
+});
 
 export default Community;
