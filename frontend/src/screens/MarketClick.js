d1439ecf0a6a28f0f44ab608c332980e51c049f3
MoonInbae <mib010213@gmail.com>
2022-06-14 15:50:39 +0900

    CommunityClick, MarketClick, JobClick 생성
    CommunityClick, MarketClick, JobClick 생성

---
+++ frontend/src/screens/MarketClick.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import styled from 'styled-components/native';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #E7EBF4;
+`;
+
+const MarketClick = () => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+};
+
+export default MarketClick;
