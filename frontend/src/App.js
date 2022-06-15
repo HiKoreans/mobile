bbe52a363662d2d70bff9c206546c3a67e4b2825
sojungpp <sophie2458@naver.com>
2022-06-16 02:50:16 +0900

    메인 네비  및 디자인

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,8 +18,8 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="로그인">
-        <Stack.Screen name="로그인" component={LoginScreen} />
-        <Stack.Screen name="회원가입" component={SignUp} />
+        <Stack.Screen name="로그인" component={LoginScreen} options={{headerShown: false,}}/>
+        <Stack.Screen name="회원가입" component={SignUp} options={{headerShown: false,}}/>
         <Stack.Screen name="HiKoreans" component={HiKoreans} options={{headerShown: false,}}/>
         <Stack.Screen name="동네생활 글 페이지" component={CommunityClick} options={{headerShown: false,}}/>
         <Stack.Screen name="벼룩시장 글 페이지" component={MarketClick} options={{headerShown: false,}}/>
